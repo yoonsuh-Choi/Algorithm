d8884120fb544488a26d1e09c983d89afa3fee11
yoonsuh-Choi <84033698+yoonsuh-Choi@users.noreply.github.com>
2023-08-02 12:44:13 +0900

    [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
@@ -1,23 +1,27 @@
 def solution(numbers, k):
     answer = 0
     temp = []
-    #공을 던지는 인덱스 : 0, 2, 4, 6...
-    # 1-3-5-1-3-5
-    # 1-3-2-1
     
-    numbers2 = numbers * k #다듬기 필요
+    if (2*k-1) % len(numbers) == 0:
+        answer = len(numbers)
 
-    idx = 0
-    while len(temp) < k:  
-        # idx = idx % len(numbers) #나머지
-        if idx % 2 == 0: #짝수면 공을 던지는 인덱스에 포함이 됨
-            temp.append(numbers2[idx])
-            idx += 1
-        else:
-            idx += 1
-            continue
-            
-    print(temp)        
-        
-    answer = temp[-1]
+    else:
+        answer = (2*k-1) % len(numbers)
+
+
+#     --------------------------내가 푼것-----------------
+#     numbers2 = numbers * k #다듬기 필요
+
+#     idx = 0
+#     while len(temp) < k:  
+#         # idx = idx % len(numbers) #나머지
+#         if idx % 2 == 0: #짝수면 공을 던지는 인덱스에 포함이 됨
+#             temp.append(numbers2[idx])
+#             idx += 1
+#         else:
+#             idx += 1
+#             continue
+                    
+#     answer = temp[-1]
+
     return answer
