cefff0d94b65b472cdc6923c253a1db6b205c139
yoonsuh-Choi <84033698+yoonsuh-Choi@users.noreply.github.com>
2024-01-20 20:17:00 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -2,11 +2,8 @@ def solution(n):
     answer = 0
     fac = 1
     i = 1
-    while True:
+    while fac <= n:
+        i += 1
         fac = fac * i
-        if fac > n:
-            break
-        else:
-            i += 1
     answer = i - 1
     return answer
